2962681dba63b6378d2578d7c36e2906e215f759
xharf <123190123@student.upnyk.ac.id>
2022-12-21 00:14:00 +0700

    change model and add new endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 from pydantic import BaseModel
 
@@ -12,9 +12,20 @@ import io
 from keras.utils import img_to_array
 from preprocessing import preprocessing
 from aksara_models import AksaraJawaModels
+import base64
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 # coba
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Item2(BaseModel):
@@ -45,7 +56,33 @@ def getPrediction(item: Item2):
         }
 
 
-@app.post("/*")
+class Base64img(BaseModel):
+    imgbase64: str
+
+
+@app.post("/predict-image")
+def getPredictionImage(item: Base64img):
+    try:
+        if item.imgbase64:
+            image_b64 = item.imgbase64.split(",")[1]
+            binary = base64.b64decode(image_b64)
+            img = Image.open(io.BytesIO(binary))
+            images = preprocessing(img)
+
+            return models.aksara_predict(images)
+        else:
+            return {
+                "code": 400,
+                "message": "Gambar tidak boleh kosong",
+            }
+    except Exception as e:
+        return {
+            "code": 500,
+            "message": str(e),
+        }
+
+
+@ app.post("/*")
 def notFound():
     return {
         "code": 404,
